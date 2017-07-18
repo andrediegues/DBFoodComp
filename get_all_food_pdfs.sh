5ff1ac6c2eaf75a6f10676b310896d7700b0b442
andrediegues <andrediegues@github.com>
2017-07-18 19:30:56 +0100

    changed some stuff

--- get_all_food_pdfs.sh
+++ get_all_food_pdfs.sh
@@ -2,7 +2,7 @@
 
 for i in  `seq 1 1132`;
 do
-    curl --cookie-jar "/home/andre/shitcookie" --cookie "/home/andre/shitcookie"  -O https://insa.foodcase.ch/foodcomp/pdf?$i -k
+    curl --cookie-jar "/home/andre/ptcookie" --cookie "/home/andre/ptcookie"  -O https://insa.foodcase.ch/foodcomp/pdf?$i -k
     pdftotext -layout pdf?$i
     rm -f pdf\?$i
 done
